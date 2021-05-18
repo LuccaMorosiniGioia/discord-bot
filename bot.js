d60c3ad231dbd4e9d60fe6d911cc922fe227b02a
Lucca Morosini Gioia <56040043+LuccaMorosiniGioia@users.noreply.github.com>
2021-05-18 11:12:58 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
@@ -19,6 +20,10 @@ const x5 = require("./commands/x5.js");
 const mentions = require("./commands/mentions.js");
 const mute = require("./commands/mute.js");
 const unmute = require("./commands/unmute.js");
+const rank = require("./commands/rank.js");
+const team  = require("./commands/team.js");
+const playerRank = require("./commands/playerRank.js");
+const news = require("./commands/news.js");
 
 const commands = {
     ping,
@@ -27,7 +32,11 @@ const commands = {
     x5, 
     mentions, 
     mute,
-    unmute
+    unmute,
+    rank,
+    team,
+    playerRank,
+    news
 };
 
 function commandHandler(msg){
