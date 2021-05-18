e4cc4fe43d899aea748ec680e7768cd532c3a42d
Lucca Morosini Gioia <56040043+LuccaMorosiniGioia@users.noreply.github.com>
2021-05-18 11:17:13 -0300

    Delete unmute.js

--- unmute.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(msg, args){
-    const user = msg.mentions.users.first();
-    if (!user) return msg.reply('Comando inválido');
-    else {
-        let person = msg.guild.member(user);
-        await person.edit({mute: false});
-    }
-}
