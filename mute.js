3ba32222ad376b7bbfc063fc9e11c0dd95a25d56
Lucca Morosini Gioia <56040043+LuccaMorosiniGioia@users.noreply.github.com>
2021-05-18 11:18:09 -0300

    Delete mute.js

--- mute.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function(msg, args){
-    const user = msg.mentions.users.first();
-    if (!user) return msg.reply('Comando inválido');
-    else {
-        msg.channel.send("Cala a boca, marreco");
-        let person = msg.guild.member(user);
-        await person.edit({mute: true});
-    }
-}
